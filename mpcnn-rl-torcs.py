d4ace9b14d319641965ae3836ddd7da2965cf755
Rachel A. St Clair <rachel.aileen.st@gmail.com>
2021-07-23 13:52:33 -0400

    Add files via upload

--- mpcnn-rl-torcs.py
+++ mpcnn-rl-torcs.py
@@ -11,29 +11,11 @@ import torch.nn as nn
 import matplotlib.pyplot as plt
 import numpy as np
 
-#
-batch_size = 4
-inp_size = 64
-
-#
-
-# transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
-#
-# trainset = torchvision.datasets.CIFAR10(root='./data', train=True,download=True, transform=transform)
-# trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuffle=True, num_workers=2)
-#
-# testset = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
-# testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size, shuffle=False, num_workers=2)
-#
-# classes = ('plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
-#
-
-# def imshow(img):
-#     img = img / 2 + 0.5     # unnormalize
-#     npimg = img.numpy()
-#     plt.imshow(np.transpose(npimg, (1, 2, 0)))
-#     plt.show()
-
+###hyperparams
+thresh = .8
+n = 33
+ps = 100
+mutR = 0.8
 #
 
 
@@ -48,10 +30,6 @@ class MPCNN(nn.Module):
         self.conv3 = nn.Conv2d(10, 3, 2)
         self.pool3 = nn.MaxPool2d(2)
         self.f  = nn.ReLU()
-        # self.fc1 = nn.Linear(16 * 5 * 5, 120)
-        # self.fc2 = nn.Linear(120, 84)
-        # self.fc3 = nn.Linear(84, 10)
-
     def forward(self, x):
         x = self.pool(self.conv1(x))
         x = self.pool2(self.f(self.conv2(x)))
@@ -59,31 +37,30 @@ class MPCNN(nn.Module):
         x = self.pool3(self.f(self.conv3(x)))
         return x
 
-
 mpcnn = MPCNN()
 
+def fitK(x):
+    Fnorm = nn.functional.normalize(x)
+    Fdif = [nn.functional.pairwise_distance(Fnorm[i],Fnorm[i+1]) for i in range(len(Fnorm))]
+    D = torch.abs(Fdif)
+    return torch.min(D) + torch.mean(D)
 
 
-
-F = net(inputs)
-
-Fnorm = nn.functional.normalize(F)
-
-Fdif = nn.functional.pairwise_distance(F[i for i in range(len(F))], F[j for j in range(len(F)-1)])
-
-D = torch.abs([Fdif])
-
-fit_K = torch.min(D) + torch.mean(D)
 
 
 ##evolve SRN controller in parallel
 class SRN(nn.Module):
-    def__init__(self):
+    def __init__(self):
         super().__init__()
-        self.hidden = nn.RNN(3, 3, 1) #tanh activation built-in *paper uses sigmoid*
+        self.state = None
+        self.hiddenrec = nn.Linear(6, 3, bias=True) #tanh activation built-in *paper uses sigmoid*
+        self.outlayer = nn.Linear(3, 3, bias=True)
     def forward(self, x): #input: tensor of shape (L,N,Hin)(L, N, H_{in})(L,N,Hin​)
-        x = self.hidden(x) #output: tensor of shape (L,N,D∗Hout)(L, N, D * H_{out})(L,N,D∗Hout​)
-        return x
+        if self.state is None:
+            self.state = torch.zeros_like(x)
+        self.state = self.hiddenrec(torch.cat(self.state,x)) #output: tensor of shape (L,N,D∗Hout)(L, N, D * H_{out})(L,N,D∗Hout​)
+        out = self.outlayer(self.state)
+        return out
 
 srn = SRN()
 
@@ -93,14 +70,7 @@ srn = SRN()
 #throttle = x[2]
 
 
-
-
 ##CoSyNE encoding
-thresh =
-n = 33
-ps = 100
-mutR = 0.8
-
 def make_C(xt): #xt=control signal for num simulation control steps
     return (1/(3*xt.shape(1)))*(torch.sum(torch.sum(torch.pairwise_distance(xt[i for i in range(len(xt))], xt[i for i in range(len(xt))]).square())))
 
@@ -136,19 +106,11 @@ def recomb(p, eval, mutR):
     return children
 
 
-""" where f (xi j ) is the fitness of subgenotype (weight) xi j , and f minj and f maxj are, respectively, the fitness
-of the least and most fit individuals in subpopulation i. In this case, the probability of disrupting the
-network x j is inversely proportional to its relative fitness, so that weight combinations that receive
-high fitness are more likely to be preserved, while those with low fitness are more likely to be
-disrupted and their constituents used to search for new complete solutions. In the experiments below,
-the simpler function that permutes all weights, except for the newly inserted offspring weights, was
-found to work well. """
-
-def CoSyNE(p, thresh): #p = population x weights (nxm)=33x100
+def CoSyNE(p, thresh, fitness_FX): #p = population x weights (nxm)=33x100
     for g in range(generations):
         evals = []
         for j in range(ps):
-            eval = fitCos(p[:,j]) #form complete solution and evaluate with fitness
+            eval = fitness_FX(p[:,j]) #form complete solution and evaluate with fitness
             evals.append(eval)
         if evals.any() >= thresh:
             break
@@ -164,6 +126,16 @@ def CoSyNE(p, thresh): #p = population x weights (nxm)=33x100
 
 ####run
 ##evolve mpcnn seperately according to fit_K
+
+data_sample = torch.rand(24,1,64,64)
+feature_out = mpcnn(data_sample)
+
+f = {}
+for name, param in mpcnn.named_parameters():
+    f.update({name: param})
+
+w, eval = CoSyNE(mpcnn.parameters(), thresh, fitK)
+
 ##evolve mpcnn:srn together according fitcos
 
 
@@ -171,34 +143,10 @@ def CoSyNE(p, thresh): #p = population x weights (nxm)=33x100
 
 
 ####Vestigule codes
-# criterion = nn.CrossEntropyLoss()
-# optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
-#
-#
-# for epoch in range(2):  # loop over the dataset multiple times
-#
-#     running_loss = 0.0
+
 #     for i, data in enumerate(trainloader, 0):
-#         # get the inputs; data is a list of [inputs, labels]
 #         inputs, labels = data
-#
-#         # zero the parameter gradients
-#         optimizer.zero_grad()
-#
-#         # forward + backward + optimize
 #         outputs = net(inputs)
-#         loss = criterion(outputs, labels)
-#         loss.backward()
-#         optimizer.step()
-#
-#         # print statistics
-#         running_loss += loss.item()
-#         if i % 2000 == 1999:    # print every 2000 mini-batches
-#             print('[%d, %5d] loss: %.3f' %
-#                   (epoch + 1, i + 1, running_loss / 2000))
-#             running_loss = 0.0
-#
-# print('Finished Training')
 #
 # PATH = './cifar_net.pth'
 # torch.save(net.state_dict(), PATH)
